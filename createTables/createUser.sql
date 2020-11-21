087d100506fe646a73030ee0f626a120716dc250
Am <amnuntita19@gmail.com>
2020-11-21 13:57:06 +0700

    set email to be UNIQUE

--- createTables/createUser.sql
+++ createTables/createUser.sql
@@ -2,7 +2,7 @@ CREATE TABLE user(
 	user_id  int NOT NULL auto_increment,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
+    email varchar(50) NOT NULL UNIQUE,
     password varchar(20) NOT NULL,
 	profile_pic_URL varchar(255),
     primary key(user_id)
