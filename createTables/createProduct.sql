c89b421cee49a56b44694104e53104ea34ad7442
KoFeeBriAN <coffeetk2017@gmail.com>
2020-11-21 19:16:07 +0700

    mod data, sql table(no rev, addr)

--- createTables/createProduct.sql
+++ createTables/createProduct.sql
@@ -1,13 +1,13 @@
 CREATE TABLE product(
 
 	product_id int  not null auto_increment,
-    seller_id int not null,
+  seller_id int not null,
 	product_title VARCHAR(50) not null,
 	product_detail TEXT(500),
 	price INT not null,
 	amount INT not null,
 	avg_rating FLOAT(2,1) not null,
-    update_at DATETIME not null, 
-    primary key(product_id) 
+  update_at DATETIME not null DEFAULT CURRENT_TIMESTAMP, 
+  primary key(product_id) 
 
 );
