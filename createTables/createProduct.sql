848a76c210c8c061b56f3c3388eaf6256ae63fa3
KoFeeBriAN <coffeetk2017@gmail.com>
2020-11-21 18:55:43 +0700

    add sh script, add data(not complete)

--- createTables/createProduct.sql
+++ createTables/createProduct.sql
@@ -3,9 +3,9 @@ CREATE TABLE product(
 	product_id int  not null auto_increment,
     seller_id int not null,
 	product_title VARCHAR(50) not null,
+	product_detail TEXT(500),
 	price INT not null,
 	amount INT not null,
-	product_detail TEXT(500),
 	avg_rating FLOAT(2,1) not null,
     update_at DATETIME not null, 
     primary key(product_id) 
---
+++ setup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+MYSQL_USER=root
+MYSQL_HOST=localhost
+MYSQL_PASSWORD=Ys34_Kc9
+MYSQL_DATABASE=pugsod
+
+# setup database
+mysql --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} << EOF
+  -- SHOW FULL PROCESSLIST;
+
+  -- create database if not exist, then use it.
+  CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+  USE ${MYSQL_DATABASE};
+
+  -- set foreign key check = 0 (turn off).
+  SET FOREIGN_KEY_CHECKS = 0;
+
+  -- drop all tables if exists
+  DROP TABLE IF EXISTS \
+    user, client, customer, seller, \
+    admin, sponsor, address, \
+    ordert, orderItem, \
+    product, pictureURL, productCategory, category,\
+    promotion, payment, report, review, \
+    sellerAdvertisement, sponsorAdvertisement, \
+    shopContact;
+
+  -- set foreign key check back to 1
+  SET FOREIGN_KEY_CHECKS = 1;
+EOF
+
+# execute createTable script
+for file in "./createTables"/*.sql
+do
+  if [[ -f $file ]]; then
+    mysql --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < ${file}
+  fi 
+done
+
+# execute alter
+for file in "./alter"/*.sql
+do
+  if [[ -f $file ]]; then
+    mysql --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < ${file}
+  fi 
+done
+
+mysql --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} << EOF
+
+SET GLOBAL local_infile=1;
+SET FOREIGN_KEY_CHECKS = 0;
+
+EOF
+
+echo $PWD
+
+# import csv files to tables
+for file_csv in "${PWD}/data"/*.csv
+do
+  filename="$(basename ${file_csv} .csv)"
+  echo $file_csv
+  echo $filename
+  if [[ -f $file_csv ]]; then
+    mysql --silent --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}<< EOF
+
+      SET FOREIGN_KEY_CHECKS = 0;    
+      LOAD DATA LOCAL INFILE "${file_csv}" 
+      INTO TABLE ${filename} 
+      FIELDS TERMINATED BY ',' 
+      LINES TERMINATED BY "\n" 
+      IGNORE 1 ROWS;
+      SET FOREIGN_KEY_CHECKS = 1;    
+EOF
+  fi
+
+done
+
+mysql --local-infile=1 --show-warnings -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} << EOF
+
+SET GLOBAL local_infile=0;
+SET FOREIGN_KEY_CHECKS = 1;
+
+EOF
